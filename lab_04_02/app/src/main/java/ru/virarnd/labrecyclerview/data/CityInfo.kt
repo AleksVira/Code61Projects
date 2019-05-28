d9c8b7f5691b8394cc334c52ec5c4ef1eeb72893
Aleksey <123all@newmail.ru>
2019-05-28 19:30:11 +0300

    small commit

--- lab_04_02/app/src/main/java/ru/virarnd/labrecyclerview/data/CityInfo.kt
+++ lab_04_02/app/src/main/java/ru/virarnd/labrecyclerview/data/CityInfo.kt
@@ -1,2 +1,3 @@
 package ru.virarnd.labrecyclerview.data
 
+data class CityInfo(val cityName: String, val country: String)
--- lab_04_02/app/src/main/java/ru/virarnd/labrecyclerview/view/MainActivity.kt
+++ lab_04_02/app/src/main/java/ru/virarnd/labrecyclerview/view/MainActivity.kt
@@ -2,12 +2,25 @@ package ru.virarnd.labrecyclerview.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import ru.virarnd.labrecyclerview.R
+import ru.virarnd.labrecyclerview.databinding.ActivityMainBinding
+import ru.virarnd.labrecyclerview.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
+
     }
 }
